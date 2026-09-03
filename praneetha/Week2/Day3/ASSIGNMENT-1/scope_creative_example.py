cb44de73c7abb2cafcada1aa8ca47c03b461a0f1
praneethaimandivalucreed <praneetha.imandivaluecreed@gmail.com>
2026-09-03 16:19:19 +0530

    added assignment and day3 files

---
+++ praneetha/Week2/Day3/ASSIGNMENT-1/scope_creative_example.py
@@ -0,0 +1,17 @@
+# In the real world, our hero is Nani
+hero = "Nani"
+
+
+def eega_transformation():
+    
+    # Inside this special world, the hero is reborn as a fly
+    hero = "Eega"
+
+    print(f"Inside the transformation world, hero is: {hero}")
+
+
+print(f"Before transformation, hero is: {hero}")
+
+eega_transformation()
+
+print(f"After returning to the real world, hero is still: {hero}")
