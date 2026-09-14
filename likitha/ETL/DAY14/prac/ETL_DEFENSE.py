2a0a4c30771c1fa3a7a5425dad4c2c38cfefc3bf
likhitasingireddyvaluecreed <likhita.singireddyvaluecreed@gmail.com>
2026-09-14 16:17:20 +0530

    DAY14

--- likitha/ETL/DAY14/prac/ETL_DEFENSE.py
+++ likitha/ETL/DAY14/prac/ETL_DEFENSE.py
@@ -3,22 +3,29 @@ record = {
     "price": "500",
     "quantity": "2"
 }
-
-if record.get("product_id") is None:
-    raise ValueError("No product_id value")
-if record.get("price") is None:
-    raise ValueError("No price value")
-if record.get("quantity") is None:
-    raise ValueError("No quantity value")
 try:
-    price = float(record["price"])
-    quantity = int(record["quantity"])
+    if record.get("product_id") is None:
+        raise ValueError("No product_id value")
+    if record.get("price") is None:
+        raise ValueError("No price value")
+    if record.get("quantity") is None:
+        raise ValueError("No quantity value")
+    try:
+        price = float(record["price"])
+        quantity = int(record["quantity"])
+    except ValueError as e:
+        print(e)
+
+    if price < 0:
+        raise ValueError("Price cannot be negative")
+
+    if quantity <= 0:
+        raise ValueError("Quantity must be greater than 0")
+    total = price * quantity
 except ValueError as e:
     print(e)
 
-total = price * quantity
 try:
-
     insert_into_database(
     record["product_id"],
     price,
